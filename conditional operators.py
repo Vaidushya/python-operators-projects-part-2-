0754d6c239f999f047114586afc45e60e290976f
Vaidushya <anugya2014@gmail.com>
2025-03-30 14:33:07 +0530

    conditional operators.py

---
+++ conditional operators.py
@@ -0,0 +1,13 @@
+#below statement takes input from user
+number = int(input("Enter a number here: "))
+
+#checking the condition (true or false)
+if number>0:
+    print(number,"is a positive number")
+elif number==0:
+    print(number,"is a neutral number")
+else:
+    print(number,"is a negative number")
+
+a =True
+print(not a)
